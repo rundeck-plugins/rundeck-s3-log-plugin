5018a5223244f22702ab49f804c402587943fb86
Greg Schueler <greg.schueler@gmail.com>
2013-06-11 12:12:22 -0700

    Working version with classloader tweaks for rd plugins

--- src/main/java/org/rundeck/plugins/S3LogFileStoragePlugin.java
+++ src/main/java/org/rundeck/plugins/S3LogFileStoragePlugin.java
@@ -3,12 +3,10 @@ package org.rundeck.plugins;
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
+import com.amazonaws.regions.RegionUtils;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.PutObjectResult;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.*;
 import com.dtolabs.rundeck.core.logging.LogFileState;
 import com.dtolabs.rundeck.core.plugins.Plugin;
 import com.dtolabs.rundeck.plugins.ServiceNameConstants;
@@ -17,9 +15,8 @@ import com.dtolabs.rundeck.plugins.descriptions.PluginProperty;
 import com.dtolabs.rundeck.plugins.logging.LogFileStoragePlugin;
 import com.dtolabs.utils.Streams;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -46,7 +43,7 @@ public class S3LogFileStoragePlugin implements LogFileStoragePlugin {
             "store a log file. You can use these" +
             " expansion variables: ($ID = execution ID, $PROJECT = project name, $JOB = job name, " +
             "$GROUP = group name, " +
-            "$JID = job ID). Default: /logs/$ID", defaultValue = "/logs/$ID")
+            "$JID = job ID). Default: logs/$ID", defaultValue = "logs/$ID")
     private String pathFormat;
 
     @PluginProperty(title = "S3 Region", description = "AWS S3 Region to use.  You can use one of the supported " +
@@ -60,20 +57,26 @@ public class S3LogFileStoragePlugin implements LogFileStoragePlugin {
 
     private AmazonS3 amazonS3;
 
-    public void initialize(Map<String, ? extends Object> context) {
+    private Map<String, ? extends Object> context;
 
-        expandedPathFormat = expandFormat(pathFormat, context);
+    public void initialize(Map<String, ? extends Object> context) {
+        this.context = context;
+        expandedPathFormat = expandFormat(getPathFormat(), context);
 
         amazonS3 = new AmazonS3Client(new AWSCredentials() {
             public String getAWSAccessKeyId() {
-                return awsAccessKey;
+                return getAwsAccessKey();
             }
 
             public String getAWSSecretKey() {
-                return awsSecretKey;
+                return getAwsSecretKey();
             }
         });
-        Region awsregion = Region.getRegion(Regions.valueOf(region));
+        Region awsregion = RegionUtils.getRegion(getRegion());
+        if (null == awsregion) {
+            throw new IllegalStateException("Region was not found: " + getRegion());
+        }
+
         amazonS3.setRegion(awsregion);
 
     }
@@ -87,7 +90,7 @@ public class S3LogFileStoragePlugin implements LogFileStoragePlugin {
      * @return
      */
     private String expandFormat(String pathFormat, Map<String, ? extends Object> context) {
-        String result = pathFormat;
+        String result = pathFormat.replaceAll("^/+", "");
         result = result.replaceAll("\\$ID", context.get("execid").toString());
         Object id = context.get("id");
         Object jobName = context.get("name");
@@ -96,26 +99,81 @@ public class S3LogFileStoragePlugin implements LogFileStoragePlugin {
         result = result.replaceAll("\\$JOB", null != jobName ? jobName.toString() : "");
         result = result.replaceAll("\\$GROUP", null != jobGroup ? jobGroup.toString() : "");
         result = result.replaceAll("\\$PROJECT", context.get("project").toString());
-        result = result.replaceAll("\\$USER", context.get("user").toString());
+        result = result.replaceAll("\\$USER", context.get("username").toString());
 
         return result;
     }
 
     public LogFileState getState() {
-        return null;
+        LogFileState state = LogFileState.NOT_FOUND;
+
+        GetObjectMetadataRequest getObjectRequest = new GetObjectMetadataRequest(getBucket(), expandedPathFormat);
+        logger.log(Level.FINE, "getState for S3 bucket {0}:{1}", new Object[]{getBucket(), expandedPathFormat});
+        try {
+            ObjectMetadata objectMetadata = amazonS3.getObjectMetadata(getObjectRequest);
+            String metaId = objectMetadata.getUserMetadata().get("rundeck.execid");
+            logger.log(Level.FINE, "Metadata {0}", new Object[]{objectMetadata.getUserMetadata()});
+            boolean matchesId = context.get("execid").equals(metaId);
+            if(!matchesId) {
+                logger.log(Level.WARNING, "S3 Object metadata 'rundeck.execid' was not expected: {0}, expected {1}",
+                        new Object[]{metaId, context.get("execid")});
+            }
+            return LogFileState.AVAILABLE;
+        } catch (AmazonS3Exception e) {
+            if(e.getStatusCode()==404) {
+                //not found
+                logger.log(Level.FINE, "getState: S3 Object not found for {0}", expandedPathFormat);
+            }else{
+                logger.log(Level.SEVERE, e.getMessage());
+                logger.log(Level.FINE, e.getMessage(), e);
+            }
+        } catch (AmazonClientException e) {
+            logger.log(Level.SEVERE, e.getMessage());
+            logger.log(Level.FINE, e.getMessage(), e);
+        }
+
+        return state;
     }
 
     public boolean storeLogFile(InputStream stream) throws IOException {
         PutObjectResult putObjectResult = null;
         boolean success = false;
+        File tempfile = File.createTempFile("S3LogFileStoragePlugin", "temp");
+        tempfile.deleteOnExit();
         try {
-            putObjectResult = amazonS3.putObject(bucket, expandedPathFormat, stream, null);
+            FileOutputStream out = new FileOutputStream(tempfile);
+            try {
+                Streams.copyStream(stream, out);
+            } finally {
+                out.close();
+            }
+            logger.log(Level.SEVERE, "Storing content to S3 bucket {0} path {1}", new Object[]{getBucket(),
+                    expandedPathFormat});
+            PutObjectRequest putObjectRequest = new PutObjectRequest(getBucket(), expandedPathFormat, tempfile);
+            putObjectResult = amazonS3.putObject(putObjectRequest.withMetadata(createObjectMetadata()));
             success = true;
         } catch (AmazonClientException e) {
             logger.log(Level.SEVERE, e.getMessage(), e);
+        } finally {
+            if (tempfile.exists()) {
+                tempfile.delete();
+            }
         }
 
         return success;
+    }
+
+    private static final String[] STORED_META = new String[]{"execid", "username", "project"};
+
+    private ObjectMetadata createObjectMetadata() {
+        ObjectMetadata metadata = new ObjectMetadata();
+        for (String s : STORED_META) {
+            Object v = context.get(s);
+            if (null != v) {
+                metadata.addUserMetadata("rundeck." + s, v.toString());
+            }
+        }
+        return metadata;
     }
 
 
@@ -123,7 +181,7 @@ public class S3LogFileStoragePlugin implements LogFileStoragePlugin {
         S3Object object = null;
         boolean success = false;
         try {
-            object = amazonS3.getObject(bucket, expandedPathFormat);
+            object = amazonS3.getObject(getBucket(), expandedPathFormat);
             S3ObjectInputStream objectContent = object.getObjectContent();
             try {
                 Streams.copyStream(objectContent, stream);
@@ -136,5 +194,76 @@ public class S3LogFileStoragePlugin implements LogFileStoragePlugin {
         }
 
         return success;
+    }
+
+    public static void main(String[] args) throws IOException {
+        S3LogFileStoragePlugin s3LogFileStoragePlugin = new S3LogFileStoragePlugin();
+        String action = args[0];
+        Map<String, Object> context = new HashMap<String, Object>();
+        context.put("project", "test");
+        context.put("execid", "165");
+        context.put("name", "test job");
+        context.put("group", "test group");
+        context.put("id", "testjobid");
+        context.put("username", "testuser");
+        if ("state".equals(action)) {
+            context.put("execid", args[2]);
+        }
+
+        s3LogFileStoragePlugin.setAwsAccessKey("AKIAJ63ESPDAOS5FKWNQ");
+        s3LogFileStoragePlugin.setAwsSecretKey(args[1]);
+        s3LogFileStoragePlugin.setBucket("test-rundeck-logs");
+        s3LogFileStoragePlugin.setPathFormat("logs/$PROJECT/$ID.log");
+        s3LogFileStoragePlugin.setRegion("us-east-1");
+
+        s3LogFileStoragePlugin.initialize(context);
+
+        if ("store".equals(action)) {
+            s3LogFileStoragePlugin.storeLogFile(new FileInputStream(new File(args[2])));
+        } else if ("retrieve".equals(action)) {
+            s3LogFileStoragePlugin.retrieveLogFile(new FileOutputStream(new File(args[2])));
+        } else if ("state".equals(action)) {
+            System.out.println(s3LogFileStoragePlugin.getState());
+        }
+    }
+
+    public String getAwsAccessKey() {
+        return awsAccessKey;
+    }
+
+    public void setAwsAccessKey(String awsAccessKey) {
+        this.awsAccessKey = awsAccessKey;
+    }
+
+    public String getAwsSecretKey() {
+        return awsSecretKey;
+    }
+
+    public void setAwsSecretKey(String awsSecretKey) {
+        this.awsSecretKey = awsSecretKey;
+    }
+
+    public String getBucket() {
+        return bucket;
+    }
+
+    public void setBucket(String bucket) {
+        this.bucket = bucket;
+    }
+
+    public String getPathFormat() {
+        return pathFormat;
+    }
+
+    public void setPathFormat(String pathFormat) {
+        this.pathFormat = pathFormat;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
     }
 }
