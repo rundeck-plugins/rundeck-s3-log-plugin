f951a3ebc89fdea2282c69a502e3bf09dc708fb1
Greg Schueler <greg.schueler@gmail.com>
2013-12-12 15:38:53 -0800

    Update for rundeck 2.0.0 WIP

--- src/main/java/org/rundeck/plugins/S3LogFileStoragePlugin.java
+++ src/main/java/org/rundeck/plugins/S3LogFileStoragePlugin.java
@@ -8,13 +8,13 @@ import com.amazonaws.regions.RegionUtils;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.*;
+import com.dtolabs.rundeck.core.logging.ExecutionFileStorageException;
 import com.dtolabs.rundeck.core.logging.LogFileState;
-import com.dtolabs.rundeck.core.logging.LogFileStorageException;
 import com.dtolabs.rundeck.core.plugins.Plugin;
 import com.dtolabs.rundeck.plugins.ServiceNameConstants;
 import com.dtolabs.rundeck.plugins.descriptions.PluginDescription;
 import com.dtolabs.rundeck.plugins.descriptions.PluginProperty;
-import com.dtolabs.rundeck.plugins.logging.LogFileStoragePlugin;
+import com.dtolabs.rundeck.plugins.logging.ExecutionFileStoragePlugin;
 import com.dtolabs.utils.Streams;
 
 import java.io.*;
@@ -26,13 +26,14 @@ import java.util.logging.Logger;
 
 
 /**
- * {@link LogFileStoragePlugin} that stores files to Amazon S3.
+ * {@link com.dtolabs.rundeck.plugins.logging.ExecutionFileStoragePlugin} that stores files to Amazon S3.
  */
 @Plugin(service = ServiceNameConstants.LogFileStorage, name = "org.rundeck.amazon-s3")
 @PluginDescription(title = "S3", description = "Stores log files into an S3 bucket")
-public class S3LogFileStoragePlugin implements LogFileStoragePlugin, AWSCredentials {
+public class S3LogFileStoragePlugin implements ExecutionFileStoragePlugin, AWSCredentials {
 
-    public static final String DEFAULT_PATH_FORMAT = "project/${job.project}/${job.execid}.rdlog";
+    private static final String DEFAULT_FILE_SPEC = "${job.execid}.${filetype}";
+    public static final String DEFAULT_PATH_FORMAT = "project/${job.project}/"+DEFAULT_FILE_SPEC;
     public static final String DEFAULT_REGION = "us-east-1";
 
     Logger logger = Logger.getLogger(S3LogFileStoragePlugin.class.getName());
@@ -56,7 +57,7 @@ public class S3LogFileStoragePlugin implements LogFileStoragePlugin, AWSCredenti
             required = true,
             description = "The path in the bucket to store a log file. You can use these " +
                     "expansion variables: (${job.execid} = execution ID, ${job.project} = project name, " +
-                    "${job.id} = job UUID (or blank)." +
+                    "${job.id} = job UUID (or blank), ${filetype} = type of file." +
                     " Default: "
                     + DEFAULT_PATH_FORMAT,
             defaultValue = DEFAULT_PATH_FORMAT)
@@ -68,8 +69,6 @@ public class S3LogFileStoragePlugin implements LogFileStoragePlugin, AWSCredenti
             required = true,
             defaultValue = DEFAULT_REGION)
     private String region;
-
-    private String expandedPath;
 
     public S3LogFileStoragePlugin() {
     }
@@ -118,11 +117,7 @@ public class S3LogFileStoragePlugin implements LogFileStoragePlugin, AWSCredenti
         if (!getPath().contains("${job.execid}") && !getPath().endsWith("/")) {
             throw new IllegalArgumentException("path must contain ${job.execid} or end with /");
         }
-        String configpath= getPath();
-        if (!configpath.contains("${job.execid}") && configpath.endsWith("/")) {
-            configpath = path + "/${job.execid}.rdlog";
-        }
-        expandedPath = expandPath(configpath, context);
+        String expandedPath=expandPath(context, null);
         if (null == expandedPath || "".equals(expandedPath.trim())) {
             throw new IllegalArgumentException("expanded value of path was empty");
         }
@@ -130,6 +125,20 @@ public class S3LogFileStoragePlugin implements LogFileStoragePlugin, AWSCredenti
             throw new IllegalArgumentException("expanded value of path must not end with /");
         }
 
+    }
+
+    /**
+     * Expand the configured path template given the context and filetype, and using a default filename spec if only a directory path is specified
+     * @param context
+     * @param filetype
+     * @return
+     */
+    private String expandPath(Map<String, ? extends Object> context, String filetype) {
+        String configpath= getPath();
+        if (!configpath.contains("${job.execid}") && configpath.endsWith("/")) {
+            configpath = getPath() + DEFAULT_FILE_SPEC;
+        }
+        return expandPath(configpath, context, filetype);
     }
 
     /**
@@ -158,14 +167,18 @@ public class S3LogFileStoragePlugin implements LogFileStoragePlugin, AWSCredenti
      * @param pathFormat
      * @param context
      *
+     * @param filetype
      * @return
      */
-    static String expandPath(String pathFormat, Map<String, ? extends Object> context) {
+    static String expandPath(String pathFormat, Map<String, ? extends Object> context, String filetype) {
         String result = pathFormat.replaceAll("^/+", "");
         if (null != context) {
             result = result.replaceAll("\\$\\{job.execid\\}", notNull(context, "execid", ""));
             result = result.replaceAll("\\$\\{job.id\\}", notNull(context, "id", ""));
             result = result.replaceAll("\\$\\{job.project\\}", notNull(context, "project", ""));
+        }
+        if (null != filetype) {
+            result = result.replaceAll("\\$\\{filetype\\}", filetype);
         }
         result = result.replaceAll("/+", "/");
 
@@ -174,20 +187,26 @@ public class S3LogFileStoragePlugin implements LogFileStoragePlugin, AWSCredenti
 
     private static String notNull(Map<String, ?> context1, String execid1, String defaultValue) {
         Object value = context1.get(execid1);
+        return notNull(defaultValue, value);
+    }
+
+    private static String notNull(String defaultValue, Object value) {
         return value != null ? value.toString() : defaultValue;
     }
 
-    public boolean isAvailable() throws LogFileStorageException {
+    public boolean isAvailable(String filetype) throws ExecutionFileStorageException {
         LogFileState state = LogFileState.NOT_FOUND;
-
-        GetObjectMetadataRequest getObjectRequest = new GetObjectMetadataRequest(getBucket(), expandedPath);
-        logger.log(Level.FINE, "getState for S3 bucket {0}:{1}", new Object[]{getBucket(), expandedPath});
+        String fileTypepath = expandPath(path, context, filetype);
+        GetObjectMetadataRequest getObjectRequest = new GetObjectMetadataRequest(getBucket(), fileTypepath);
+        logger.log(Level.FINE, "getState for S3 bucket {0}:{1}", new Object[]{getBucket(), fileTypepath});
         try {
             ObjectMetadata objectMetadata = amazonS3.getObjectMetadata(getObjectRequest);
             Map<String, String> userMetadata = objectMetadata.getUserMetadata();
             String metaId = null;
+            String metafiletype = null;
             if (null != userMetadata) {
                 metaId = userMetadata.get("rundeck.execid");
+                metafiletype = userMetadata.get("rundeck.filetype");
             }
             logger.log(Level.FINE, "Metadata {0}", new Object[]{objectMetadata.getUserMetadata()});
             //execution ID is stored in the user metadata for the object
@@ -197,37 +216,42 @@ public class S3LogFileStoragePlugin implements LogFileStoragePlugin, AWSCredenti
                 logger.log(Level.WARNING, "S3 Object metadata 'rundeck.execid' was not expected: {0}, expected {1}",
                         new Object[]{metaId, context.get("execid")});
             }
+            if(null!=metafiletype && null!=filetype && !filetype.equals(metafiletype)){
+                logger.log(Level.WARNING, "S3 Object metadata 'rundeck.filetype' was not expected: {0}, expected {1}",
+                        new Object[]{metafiletype, filetype});
+            }
             return true;
         } catch (AmazonS3Exception e) {
             if (e.getStatusCode() == 404) {
                 //not found
-                logger.log(Level.FINE, "getState: S3 Object not found for {0}", expandedPath);
+                logger.log(Level.FINE, "getState: S3 Object not found for {0}", fileTypepath);
             } else {
                 logger.log(Level.SEVERE, e.getMessage());
                 logger.log(Level.FINE, e.getMessage(), e);
-                throw new LogFileStorageException(e.getMessage(), e);
+                throw new ExecutionFileStorageException(e.getMessage(), e);
             }
         } catch (AmazonClientException e) {
             logger.log(Level.SEVERE, e.getMessage());
             logger.log(Level.FINE, e.getMessage(), e);
-            throw new LogFileStorageException(e.getMessage(), e);
+            throw new ExecutionFileStorageException(e.getMessage(), e);
         }
 
         return false;
     }
 
-    public boolean store(InputStream stream, long length, Date lastModified) throws LogFileStorageException {
+    public boolean store(String filetype, InputStream stream, long length, Date lastModified) throws ExecutionFileStorageException {
         boolean success = false;
+        String fileTypepath = expandPath(path, context, filetype);
         logger.log(Level.FINE, "Storing content to S3 bucket {0} path {1}", new Object[]{getBucket(),
-                expandedPath});
-        ObjectMetadata objectMetadata = createObjectMetadata(length, lastModified);
-        PutObjectRequest putObjectRequest = new PutObjectRequest(getBucket(), expandedPath, stream, objectMetadata);
+                fileTypepath});
+        ObjectMetadata objectMetadata = createObjectMetadata(length, lastModified, filetype);
+        PutObjectRequest putObjectRequest = new PutObjectRequest(getBucket(), fileTypepath, stream, objectMetadata);
         try {
             PutObjectResult putObjectResult = amazonS3.putObject(putObjectRequest);
             success = true;
         } catch (AmazonClientException e) {
             logger.log(Level.SEVERE, e.getMessage(), e);
-            throw new LogFileStorageException(e.getMessage(), e);
+            throw new ExecutionFileStorageException(e.getMessage(), e);
         }
         return success;
     }
@@ -238,7 +262,7 @@ public class S3LogFileStoragePlugin implements LogFileStoragePlugin, AWSCredenti
     private static final String[] STORED_META = new String[]{"execid", "username", "project", "url", "serverUrl",
             "serverUUID"};
 
-    private ObjectMetadata createObjectMetadata(long length, Date lastModified) {
+    private ObjectMetadata createObjectMetadata(long length, Date lastModified, String filetype) {
         ObjectMetadata metadata = new ObjectMetadata();
         for (String s : STORED_META) {
             Object v = context.get(s);
@@ -246,17 +270,21 @@ public class S3LogFileStoragePlugin implements LogFileStoragePlugin, AWSCredenti
                 metadata.addUserMetadata("rundeck." + s, v.toString());
             }
         }
+        if(null!=filetype){
+            metadata.addUserMetadata("rundeck.filetype", filetype);
+        }
         metadata.setLastModified(lastModified);
         metadata.setContentLength(length);
         return metadata;
     }
 
 
-    public boolean retrieve(OutputStream stream) throws IOException, LogFileStorageException {
+    public boolean retrieve(String filetype, OutputStream stream) throws IOException, ExecutionFileStorageException {
         S3Object object = null;
+        String fileTypepath = expandPath(path, context, filetype);
         boolean success = false;
         try {
-            object = amazonS3.getObject(getBucket(), expandedPath);
+            object = amazonS3.getObject(getBucket(), fileTypepath);
             S3ObjectInputStream objectContent = object.getObjectContent();
             try {
                 Streams.copyStream(objectContent, stream);
@@ -266,13 +294,13 @@ public class S3LogFileStoragePlugin implements LogFileStoragePlugin, AWSCredenti
             }
         } catch (AmazonClientException e) {
             logger.log(Level.SEVERE, e.getMessage(), e);
-            throw new LogFileStorageException(e.getMessage(), e);
+            throw new ExecutionFileStorageException(e.getMessage(), e);
         }
 
         return success;
     }
 
-    public static void main(String[] args) throws IOException, LogFileStorageException {
+    public static void main(String[] args) throws IOException, ExecutionFileStorageException {
         S3LogFileStoragePlugin s3LogFileStoragePlugin = new S3LogFileStoragePlugin();
         String action = args[0];
         Map<String, Object> context = new HashMap<String, Object>();
@@ -282,6 +310,7 @@ public class S3LogFileStoragePlugin implements LogFileStoragePlugin, AWSCredenti
         context.put("group", "test group");
         context.put("id", "testjobid");
         context.put("username", "testuser");
+        context.put("filetype", args[3]);
 
         s3LogFileStoragePlugin.setAWSAccessKeyId("AKIAJ63ESPDAOS5FKWNQ");
         s3LogFileStoragePlugin.setAWSSecretKey(args[1]);
@@ -293,11 +322,11 @@ public class S3LogFileStoragePlugin implements LogFileStoragePlugin, AWSCredenti
 
         if ("store".equals(action)) {
             File file = new File(args[3]);
-            s3LogFileStoragePlugin.store(new FileInputStream(file), file.length(), new Date(file.lastModified()));
+            s3LogFileStoragePlugin.store(args[3],new FileInputStream(file), file.length(), new Date(file.lastModified()));
         } else if ("retrieve".equals(action)) {
-            s3LogFileStoragePlugin.retrieve(new FileOutputStream(new File(args[3])));
+            s3LogFileStoragePlugin.retrieve(args[3],new FileOutputStream(new File(args[3])));
         } else if ("state".equals(action)) {
-            System.out.println("available? " + s3LogFileStoragePlugin.isAvailable());
+            System.out.println("available? " + s3LogFileStoragePlugin.isAvailable(args[3]));
         }
     }
 
